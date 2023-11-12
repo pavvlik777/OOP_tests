5ba1b875b17dbb4f5600f844e73d86da9ab74b22
pavvlik777 <pavvlik77727@gmail.com>
2023-11-12 19:35:49 +0300

    init

---
+++ OOP.TwilightSparkle/OOP.TwilightSparkle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace OOP.TwilightSparkle
+{
+    public class Startup
+    {
+        public static async Task Main()
+        {
+            return;
+        }
+    }
+}
